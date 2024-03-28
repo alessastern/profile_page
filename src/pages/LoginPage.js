ba6b6d4be06d043d952ca316bf2fd336fb5f3e60
Алиса Полубоярова <kissofmoon.da@gmail.com>
2024-03-28 19:15:11 +0300

    form done

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,21 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles.css";
 
 function LoginPage() {
-  const [username, setUsername] = useState("");
+  const [login, setLogin] = useState("");
   const [password, setPassword] = useState("");
+  const [isDisabled, setDisabled] = useState(true);
+
+  const checkValidity = (login, password) => {
+    return login === "developer21" && password === "123456"
+      ? setDisabled(false)
+      : setDisabled(true);
+  };
+
+  useEffect(() => {
+    checkValidity(login, password);
+  }, [login, password]);
 
   return (
     <div className="loginform">
       <div className="title">Log in</div>
+      <form id="login">
+        <div className="formfield">
+          <input
+            className="input"
+            placeholder="login"
+            type="text"
+            onChange={(e) => setLogin(e.target.value)}
+          />
+        </div>
+        <div className="formfield">
+          <input
+            className="input"
+            placeholder="password"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+      </form>
       <div className="formfield">
-        <input className="input" />
-      </div>
-      <div className="formfield">
-        <input className="input" />
-      </div>
-      <div className="formfield">
-        <button className="button">go!</button>
+        <button className="button" form="login" disabled={isDisabled}>
+          go!
+        </button>
       </div>
     </div>
   );
